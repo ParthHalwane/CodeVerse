0767dc3078439163579482e3761de6c2d318fdf1
Parth Halwane <107027989+ParthHalwane@users.noreply.github.com>
2025-05-17 20:52:38 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 const server = http.createServer(app)
 const io = new Server(server, {
 	cors: {
-	    origin: "https://codeverse-2.onrender.com",
+	    origin: "*",
 	    methods: ["GET", "POST"],
 	    credentials: true,
   	},
